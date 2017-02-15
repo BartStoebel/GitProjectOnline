143839918da3a7ea695a472cfa35c9726751263b
Bart.Stoebel <Bart.Stoebel@gmail.com>
2017-02-15 15:04:45 +0100

    opnieuw regel toegevoegd

--- src/gitprojecttest/GitProjectTest.java
+++ src/gitprojecttest/GitProjectTest.java
@@ -17,6 +17,7 @@ public class GitProjectTest {
     public static void main(String[] args) {
         System.out.println("Git test");
         System.out.println("hier verder gewerkt:");// TODO code application logic here
+        System.out.println("hier opnieuw verder gewerkt:");    
     }
     
 }
