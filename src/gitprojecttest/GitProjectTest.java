7d3ce716fff427118b536e7d58b99cbb32a3a1fd
Vinnie - Bart Stoebel <Bart.Stoebel@gmail.com>
2017-02-16 11:34:30 +0100

    regels verwijderd behalve WorkingOnIt

--- src/gitprojecttest/GitProjectTest.java
+++ src/gitprojecttest/GitProjectTest.java
@@ -16,22 +16,7 @@ public class GitProjectTest {
      */
     public static void main(String[] args) {
         System.out.println("Git in WorkingOnIt");
-        System.out.println("hier verder gewerkt:");
-        System.out.println("hier opnieuw verder gewerkt:");   
-        String tekst = "Dit is een stukje tekst";
-        String[] arrString = tekst.split(" ",7);
-        for (String a : arrString ){
-            System.out.println(a);
-        }
-       StringBuilder naam = new StringBuilder("Pierre");
-       System.out.println(naam.length());
-       naam.append(' ');
-       naam.insert(0, "De grote ");
-       System.out.println(naam);
-       System.out.println("WorkingOnItTestOffline");
-       
-               
-       
+        
     }
     
 }
