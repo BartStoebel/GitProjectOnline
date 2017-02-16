0e8738350f4688d67c592341128f2153fccea3ed
Vinnie - Bart Stoebel <Bart.Stoebel@gmail.com>
2017-02-16 13:58:04 +0100

    Palindroom toegevoegd met spatie in Palindroom

---
+++ src/gitprojecttest/Palindroom.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gitprojecttest;
+
+/**
+ *
+ * @author Vinnie
+ */
+public class Palindroom {
+    public boolean testPalindroom(String s){
+        boolean check = true;
+        s = s.toLowerCase();
+        int teller = 0;
+        while (teller <= s.length()-teller-1){
+            if (s.charAt(teller) != s.charAt(s.length()-teller - 1)){
+                check = false;
+            }
+            teller++;
+        }        
+        return check;
+        
+    }
+}
