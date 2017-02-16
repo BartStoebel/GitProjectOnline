32ba616223c092df9d51bad15de8c19d6c9948a8
Vinnie - Bart Stoebel <Bart.Stoebel@gmail.com>
2017-02-16 11:42:01 +0100

    klasse getal toegevoegd

---
+++ src/gitprojecttest/GetalMain.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gitprojecttest;
+
+/**
+ *
+ * @author Vinnie
+ */
+public class GetalMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Getal getal = new Getal();
+        getal.print();
+    }
+    
+}
