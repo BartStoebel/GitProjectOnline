97770f94818f3def0c488c5a5f5cb756b7449b3c
Vinnie - Bart Stoebel <Bart.Stoebel@gmail.com>
2017-02-16 14:08:44 +0100

    y toegevoegd

---
+++ src/gitprojecttest/Test.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gitprojecttest;
+
+/**
+ *
+ * @author Vinnie
+ */
+public class Test {
+    public int x,y;
+    
+}
