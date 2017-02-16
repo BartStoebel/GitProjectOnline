8728e8cd481140615ace88bac6dbd5b343325b9e
Vinnie - Bart Stoebel <Bart.Stoebel@gmail.com>
2017-02-16 13:56:18 +0100

    Palindroom toegevoegd

--- src/gitprojecttest/GitProjectTest.java
+++ src/gitprojecttest/GitProjectTest.java
@@ -20,13 +20,11 @@ public class GitProjectTest {
 //        System.out.println("Git in Master");
         System.out.println("Git in WorkingOnIt");
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Geef een rekensom in:");
-        String som = scanner.nextLine();
-        Rekenaar rekenaar = new Rekenaar();
-        
-        
-        System.out.println("Het totaal is: " + rekenaar.reken(som));
-        System.out.println("Proficiat : project geslaagd");
+        System.out.println("Geef een palindroom in:");
+        String pal = scanner.nextLine();
+        Palindroom palindroom  = new Palindroom();
+        System.out.println("Het totaal is: " + palindroom.testPalindroom(pal));
+        //System.out.println("Proficiat : project geslaagd " + pal);
 
 
     }
